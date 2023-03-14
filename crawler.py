5e26bff575697fdb5c56829c8b87f641f330d33a
Jang-DongHo <donghoo18@naver.com>
2023-03-14 12:44:50 +0900

    [Feat] 크롤러 초기 클래스 구현

--- crawler.py
+++ crawler.py
@@ -1,11 +1,24 @@
 class Crawler:
-    def __init__(self):
+    def __init__(self,
+                 departmentName_ko,
+                 departmentName_en,
+                 categoryTags):
+        self.departmentName_ko = departmentName_ko
         self.departmentName_en = departmentName_en
-        self.mi = mi
-        self.bbsId = bbsId
-  
-url = f'https://www.gnu.ac.kr/{departmentName_en}/na/ntt/selectNttList.do?mi={mi}&bbsId={bbsId}'
+        self.categoryTags = categoryTags
+        
+    def getData(self):
+        for categoryTag in self.categoryTags:
+            categoryName = categoryTag[0]
+            mi = categoryTag[1]
+            bbsId = categoryTag[2]
+            baseUrl = f'https://www.gnu.ac.kr/{self.departmentName_en}/na/ntt/selectNttList.do?mi={mi}&bbsId={bbsId}'
+            print(baseUrl)
 
-departmentName_en = 'ls';
-mi = 13180;
-bbsId = 4048;
+ls = Crawler(
+    departmentName_ko = "생명공학부",
+    departmentName_en = "ls",
+    categoryTags = [["공지사항", 13180, 4048], ["장학", 11111, 4444]]
+)
+
+ls.getData()
