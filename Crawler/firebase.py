3abc3f6897751e64740c690fe02c3e941b98ef3e
Jang-DongHo <donghoo18@naver.com>
2023-03-17 17:01:47 +0900

    [Feat] 파이어베이스 연동 테스트

---
+++ Crawler/firebase.py
@@ -0,0 +1,10 @@
+# firebase 관련 설정들을 모아둔 파일입니다.
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import os
+
+cred = credentials.Certificate(os.getcwd() + "/gnu-moa-firebase-adminsdk.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
