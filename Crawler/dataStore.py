276eced9f660e30d591dd4e10d27f110a557bb76
Yugeyong <69104743+YUkyeong1@users.noreply.github.com>
2023-03-22 14:40:50 +0900

    업데이트_1

--- Crawler/dataStore.py
+++ Crawler/dataStore.py
@@ -1,13 +1,15 @@
 from .firebase import db
+def ChkLastNum():
+    docs = db.collection('lastNoticeNum').stream()
+    doc_names = [doc.id for doc in docs]
+    lastNums = []
+    for doc_name in doc_names:
+        doc_ref = db.collection('lastNoticeNum').document(doc_name)
+        doc = doc_ref.get()
+        data = doc.to_dict()
+        lastNums.append(data.get('LastNum'))
 
-docs = db.collection('lastNoticeNum').stream()
-doc_names = [doc.id for doc in docs]
-lastNums = []
-for doc_name in doc_names:
-    doc_ref = db.collection('lastNoticeNum').document(doc_name)
-    doc = doc_ref.get()
-    data = doc.to_dict()
-    lastNums.append(data.get('LastNum'))
+    print(doc_names)
+    print(lastNums)
+    return doc_names, lastNums
 
-print(doc_names)
-print(lastNums)
--- main.py
+++ main.py
@@ -2,16 +2,34 @@ import time
 from Crawler.cns import arrcns
 from Crawler.getUrlHtml import *
 from Crawler.dataStore import *
+
 start_time = time.time()
-"""
+""" 10개 초기화
 for department in arrcns:
     i = 0
     categoryNames, baseUrls = department.getBaseUrls()
     for baseUrl in baseUrls:
         text, getPostUrls = get_posts(baseUrl)
-        saveLastNum(department, categoryNames[i], text)
         savePostInfo(categoryNames[i],department, get_title_and_context(text, getPostUrls))
-        i += 1
 """
+
+
+doc_names, lastNums = ChkLastNum()
+chkIndex = 0
+
+for department in arrcns:
+    i=0
+    categoryNames, baseUrls = department.getBaseUrls()
+    for baseUrl in baseUrls:
+        text, getPostUrls = get_posts(baseUrl)
+        print(lastNums[chkIndex], text)
+        if lastNums[chkIndex] != text:
+            num = str(int(text) - int(lastNums[chkIndex]))
+            print(doc_names[chkIndex] + " " + num + "개")
+        else:
+            print(doc_names[chkIndex] + " 변경사항 없음")
+        saveLastNum(department, categoryNames[i], text)
+        i += 1
+        chkIndex += 1
 
 print("--- 파싱 진행 시간: %s 초 ---" % (time.time() - start_time))
