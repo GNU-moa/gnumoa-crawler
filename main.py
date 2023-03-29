02c9baec1f9cde8a9706c0e4b43e48ecd8e02794
Jang-DongHo <donghoo18@naver.com>
2023-03-29 13:41:10 +0900

    [Feat] 날짜 추가

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Crawler import *
 import time
 
 start_time = time.time()
-Run(arrcns) # 자연과학대학
+#Run(arrcns) # 자연과학대학
 Run(arrinmun) # 인문대학
 print(f"--- 전체 학과 파싱 진행 시간: %s 초 ---" % (time.time() - start_time))
 
